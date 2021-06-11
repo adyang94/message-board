2fa4362feeb7430a52cc9c148cfcfc0f81649924
adyang94 <adyang94@yahoo.com>
2021-06-10 20:27:56 -0400

    adding new message page and submit btn

--- routes/index.js
+++ routes/index.js
@@ -12,19 +12,24 @@ const messages = [
     added: new Date()
   }
 ];
-
+console.log('index.js running');
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
+  console.log('get /');
   res.render('index', { title: 'Express' , messages: messages});
 });
 
-router.get('/new', function(req, res, next){
-  res.render('forms', {title: 'New Message', })
+router.get('/new', function(req, res){
+  console.log('get /new');
+  res.render('forms', {title: 'New Message', });
 })
 
 router.post('/new', function(req, res){
+  console.log('post /new');
   const messageText = req.body.message;
   const messageAuthor = req.body.author;
-})
+  messages.push({ text: messageText, user: messageAuthor, added: new Date() });
+  res.redirect('/');
+});
 
 module.exports = router;
